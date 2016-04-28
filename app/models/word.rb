72e9a466164a52f30963b05fff34ed3148eadc28
TomsMacBookPro <tomsmacbookpro@TomsMacBookPro-2.local>
2016-04-29 03:31:55 +0900

    correct codings

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,8 +1,8 @@
 class Word < ActiveRecord::Base
-	has_many :appearances, foreign_key: [:word_id], dependent: :delete:all
+	has_many :appearances, foreign_key: [:word_id], dependent: :delete_all
 	has_many :lessons, through: :appearance
-	has_many :examples, foreign_key: [:word_id], dependent: :delete:all
-	has_many :relations, foreign_key: [:word_id], dependent: :delete:all
+	has_many :examples, foreign_key: [:word_id], dependent: :delete_all
+	has_many :relations, foreign_key: [:word_id], dependent: :delete_all
 	has_many :synonym, through: :relations, source: :to_word
 
 end
