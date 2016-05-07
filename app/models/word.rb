9fa63cedf364de8dc12d3267ae77636a817ecb70
torikichi <tomsmacbookpro@TomsMacBookPro-2.local>
2016-05-07 15:38:15 +0900

    count and summary of synonyms available

--- app/models/word.rb
+++ app/models/word.rb
@@ -8,7 +8,7 @@ class Word < ActiveRecord::Base
 # 類義語(区分1)および対義語(区分2)の件数を取得する ※件数導出はviewで行うこと
   def cntSynm(kbn, wordId)
 	if kbn == 1
-	  Relation.find_by_sql(["select * from relations where syn_ant_cd=? and rel_id=(select rel_id from relations where syn_ant_cd=? and word_id=?)", kbn, kbn, wordId])
+	  Relation.find_by_sql(["select rel_id, count(rel_id) as subTotal from relations where syn_ant_cd=? and rel_id in (select rel_id from relations where syn_ant_cd=? and word_id=?) group by rel_id", kbn, kbn, wordId])
 	elsif kbn == 2
 	  Relation.find_by_sql([\
 	  	"select * from relations where syn_ant_cd=? and rel_id=(select rel_id from relations where syn_ant_cd=? and word_id=?) and ant_contrast_cd<>(select ant_contrast_cd from relations where syn_ant_cd=? and word_id=?)"\
