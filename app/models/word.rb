6a623cdc65a7421307332d5f21b05725e05cbac1
torikichi <tomsmacbookpro@TomsMacBookPro-2.local>
2016-05-07 11:07:41 +0900

    count of antonym available

--- app/models/word.rb
+++ app/models/word.rb
@@ -5,9 +5,16 @@ class Word < ActiveRecord::Base
 	has_many :relations, foreign_key: [:word_id], dependent: :delete_all
 	has_many :synonym, through: :relations, source: :from_word
 
-# 類義語(区分1)or対義語(区分2)の件数を取得する
+# 類義語(区分1)および対義語(区分2)の件数を取得する ※件数導出はviewで行うこと
   def cntSynm(kbn, wordId)
-  	Relation.find_by_sql(["select rel_id from relations where syn_ant_cd=? and rel_id=(select rel_id from relations where word_id=?)", kbn, wordId])
+	if kbn == 1
+	  Relation.find_by_sql(["select * from relations where syn_ant_cd=? and rel_id=(select rel_id from relations where syn_ant_cd=? and word_id=?)", kbn, kbn, wordId])
+	elsif kbn == 2
+	  Relation.find_by_sql([\
+	  	"select * from relations where syn_ant_cd=? and rel_id=(select rel_id from relations where syn_ant_cd=? and word_id=?) and ant_contrast_cd<>(select ant_contrast_cd from relations where syn_ant_cd=? and word_id=?)"\
+	  	, kbn ,kbn, wordId, kbn, wordId])
+	end
+
   end
 
 end
