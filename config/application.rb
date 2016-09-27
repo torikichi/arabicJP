7b04ac77554aee1bb427f03d9db827402580cb2c
torikichi <tomsmacbookpro@TomsMacBookPro-2.local>
2016-09-27 17:16:47 +0900

    fixed url setting and bug

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module ArabicJP
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.autoload_paths += %W(#{config.root}/app/validators) 
+    config.autoload_paths += %W(#{config.root}/app/validators)
+
+    config.relative_url_root = '/milaaha'
   end
 end
