311152624ebf672081921e51699c57ac41f79775
TomsMacBookPro <tomsmacbookpro@TomsMacBookPro-2.local>
2018-11-17 11:48:14 +0900

    update Gemfile.lock to fix potential security vulnerability

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,26 +4,26 @@ RSpec.describe User, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   describe "validation_test" do
     it '名前がなければエラー' do
-    	user = User.new(name: nil)
-    	user.valid?
-    	expect(user.errors[:name]).to include("が未入力です。")
-	  end
+	  user = User.new(name: nil)
+	  user.valid?
+	  expect(user.errors[:name]).to include("が未入力です。")
+	end
 
-		it '名前が重複あったらエラー' do
-			User.create(name: "john doe")
-			user = User.new(name: "john doe")
-			user.valid?
-			expect(user.errors[:name]).to include("が重複しています。")
-		end
+	it '名前が重複あったらエラー' do
+	  User.create(name: "john doe")
+	  user = User.new(name: "john doe")
+	  user.valid?
+	  expect(user.errors[:name]).to include("が重複しています。")
+	end
 
-		it '名前が50字以上はエラー' do
-		end
+	it '名前が50字以上はエラー' do
+	end
 
-		it 'メールアドレスが255字以上はエラー' do
-		end
+	it 'メールアドレスが255字以上はエラー' do
+	end
 
-		it 'メールアドレスが重複(大文字小文字は無視)あったらエラー' do
-		end
+	it 'メールアドレスが重複(大文字小文字は無視)あったらエラー' do
+	end
   end
 
   describe "downcase_test" do
